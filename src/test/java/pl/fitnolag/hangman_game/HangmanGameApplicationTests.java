cdaa99171f846669c33a31e54b7fe4fa1c95439b
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-08-24 23:17:29 +0200

    first commit

---
+++ src/test/java/pl/fitnolag/hangman_game/HangmanGameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fitnolag.hangman_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
