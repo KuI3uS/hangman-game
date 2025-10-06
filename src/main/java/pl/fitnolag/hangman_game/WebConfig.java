28f1d7c561502379f27c22585965ef20b7765abd
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-10-06 17:38:47 +0200

    vercal

--- src/main/java/pl/fitnolag/hangman_game/WebConfig.java
+++ src/main/java/pl/fitnolag/hangman_game/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://hangman-backend-ni0a.onrender.com")
+                        .allowedOrigins("https://hangman-game-one-taupe.vercel.app")
                         .allowedMethods("*");
             }
         };
