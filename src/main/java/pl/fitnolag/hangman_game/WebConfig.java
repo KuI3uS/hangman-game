1e8e59de0821852bded32ecfc408d53906fc5759
KuI3uS <kuba.marcinkowski98@wp.pl>
2025-10-06 17:37:02 +0200

    webConfig

---
+++ src/main/java/pl/fitnolag/hangman_game/WebConfig.java
@@ -0,0 +1,21 @@
+package pl.fitnolag.hangman_game;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://hangman-backend-ni0a.onrender.com")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
